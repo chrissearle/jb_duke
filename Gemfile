ab5fc0060fa2d52304c0a38879f2a2f043bfae18
Chris Searle <chris@chrissearle.org>
2010-11-30 11:16:58 +0100

    Update to twitter 1.0

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 
 gem 'twitter', '>=0.9.7'
 gem 'i18n', '>=0.4.1'
+gem 'getopt'
--- javapils.rb
+++ javapils.rb
@@ -2,13 +2,28 @@ require 'rubygems'
 require 'i18n'
 require 'twitter'
 require 'yaml'
+require 'getopt/std'
 
 config = YAML::load(File.open(File.join(File.dirname(__FILE__), 'javapils.yaml')))
 
-CONSUMER_TOKEN = config['twitter']['consumer']['token']
-CONSUMER_SECRET = config['twitter']['consumer']['secret']
-ACCESS_TOKEN = config['twitter']['access']['token']
-ACCESS_SECRET = config['twitter']['access']['secret']
+config_params = config
+
+opt = Getopt::Std.getopts("ct")
+config_params = config['test'] if opt["t"]
+
+CONSUMER_TOKEN = config_params['twitter']['consumer']['token']
+CONSUMER_SECRET = config_params['twitter']['consumer']['secret']
+ACCESS_TOKEN = config_params['twitter']['access']['token']
+ACCESS_SECRET = config_params['twitter']['access']['secret']
+
+if opt["c"]
+  puts "CONSUMER TOKEN : #{CONSUMER_TOKEN}"
+  puts "CONSUMER SECRET: #{CONSUMER_SECRET}"
+  puts "ACCESS TOKEN   : #{ACCESS_TOKEN}"
+  puts "ACCESS SECRET  : #{ACCESS_SECRET}"
+  puts "FORMAT         : #{config_params['message']['format']}"
+  exit
+end
 
 I18n.load_path << Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{yml}') ]
 
@@ -18,13 +33,17 @@ I18n.default_locale = :nb
 dow = I18n.l Time.now, :format => "%a"
 weeknum = I18n.l Time.now, :format => "%W"
 
-if ((dow == "tir") and (weeknum.to_i % 2 == 1))
-  tweet = I18n.l Time.now, :format => config['message']['format']
+if ((dow == "tir") and (weeknum.to_i % 2 == 1)) or opt["t"]
+  message = I18n.l Time.now, :format => config_params['message']['format']
   
-  oauth = Twitter::OAuth.new(CONSUMER_TOKEN, CONSUMER_SECRET)
-  oauth.authorize_from_access(ACCESS_TOKEN, ACCESS_SECRET)
+  Twitter.configure do |config|
+    config.consumer_key = CONSUMER_TOKEN
+    config.consumer_secret = CONSUMER_SECRET
+    config.oauth_token = ACCESS_TOKEN
+    config.oauth_token_secret = ACCESS_SECRET
+  end
+
+  client = Twitter::Client.new
   
-  client = Twitter::Base.new(oauth)
-  
-  client.update(tweet)
+  client.update(message)
 end
