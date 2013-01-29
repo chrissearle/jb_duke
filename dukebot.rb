b2826cd2482e0fdf853e84ce1c3b3a3b0f8350a0
Chris Searle <chris@chrissearle.org>
2013-01-29 13:15:40 +0100

    Move bot to beer class

--- dukebot.rb
+++ dukebot.rb
@@ -2,34 +2,44 @@
 
 require 'cinch'
 require 'i18n'
+require 'getopt/std'
 
-def beer?
-  weeknum = (I18n.l Time.now, :format => '%V').to_i
-  dow = (I18n.l Time.now, :format => '%u').to_i
+require File.dirname(__FILE__) + '/javabin/beer'
 
-  dow == 2 && (weeknum % 2 == 1)
+opt = Getopt::Std.getopts("t")
+
+@test = opt["t"]
+
+def get_config
+  @config ||= YAML::load(File.open(File.join(File.dirname(__FILE__), 'config.yml')))
+
+  if @test
+    @config['test']
+  else
+    @config
+  end
 end
 
-def showbeer?
-  hour = (I18n.l Time.now, :format => '%H').to_i
-  min = (I18n.l Time.now, :format => '%M').to_i
-  
-  hour == 11 && min == 19
+def get_beer
+  @beer ||= Beer.new(get_config)
 end
 
 class TimeActivatedThread
   def initialize(bot)
     @bot = bot
+    @beer = get_beer
     @bot.loggers.debug "Initialized TimeActivatedThread with bot"
   end
-  
+
   def start
     @bot.loggers.debug "Started TimeActivatedThread"
     while true
       sleep 60
-      if beer?
-        if showbeer?
-          @bot.handlers.dispatch(:time_activated, nil, "Oslo javaPils i dag - Billabong")
+      locations = @beer.beer?
+
+      locations.each do |location|
+        if @beer.show_beer? location
+          @bot.handlers.dispatch(:time_activated, nil, "#{@beer.name location} javaPils i dag - #{@beer.place location}")
         end
       end
     end
@@ -49,25 +59,32 @@ end
 
 class JavaPilsPlugin
   include Cinch::Plugin
-  
+
   match /pils\?/
 
   def execute(m)
-    if beer?
-      m.reply "Ja! Oslo javaPils i dag - Billabong"
-    else
+    beer = get_beer
+    locations = beer.beer?
+
+    locations.each do |location|
+      m.reply "Ja! #{beer.name location} javaPils i dag - #{beer.place location}"
+    end
+
+    if locations.size == 0
       m.reply "Ikke i dag :("
     end
   end
 end
 
+conf = get_config['bot']
+
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick = "jb_duke"
-    c.realname = "javaBin Duke Bot"
-    c.user = "jb_duke"
-    c.server = "irc.underworld.no"
-    c.channels = ["#java.no"]
+    c.nick = conf['nick']
+    c.realname = conf['real-name']
+    c.user = conf['user-name']
+    c.server = conf['hostname']
+    c.channels = [conf['channel']]
     c.verbose = true
     c.plugins.plugins = [TimeActivatedPlugin, JavaPilsPlugin]
   end
---
+++ javabin/beer/beer.rb
@@ -0,0 +1,96 @@
+class Beer
+  def initialize(config)
+    self.configure(config)
+  end
+
+  def configure(config)
+    @locations = config["locations"]
+  end
+
+
+  def beer?(location = :all)
+    if location == :all
+      return @locations.find_all { |l| beer_day_for_location? l[1] }.map { |l| l[0] }
+    else
+      if @locations.has_key? location
+        return [location] if beer_day_for_location?(@locations[location])
+      end
+    end
+
+    false
+  end
+
+  def place(location)
+    if @locations.has_key? location
+      return @locations[location]['place']
+    end
+
+    "Unknown location"
+  end
+
+  def name(location)
+    if @locations.has_key? location
+      return @locations[location]['display-name']
+    end
+
+    "Unknown group"
+  end
+
+  def show_beer?(location)
+    if @locations.has_key? location
+      now = Time.now
+
+      hour = (I18n.l now, :format => '%H').to_i
+      min = (I18n.l now, :format => '%M').to_i
+
+      hour == @locations[location]['announce-hour'] && min == @locations[location]['announce-min']
+    end
+
+    false
+  end
+
+  private
+
+  def beer_day_for_location?(location)
+    dow = location['day-of-week']
+    week = get_week(location)
+
+    beer_day? dow, week
+  end
+
+  def beer_day?(dow, week)
+    current_week_num = (I18n.l Time.now, :format => '%V').to_i
+    current_dow = (I18n.l Time.now, :format => '%u').to_i
+
+    valid_week = false
+
+    if week == :every
+      valid_week = true
+    end
+
+    if week == :odd
+      valid_week = current_week_num % 2 == 1
+    end
+
+    if week == :even
+      valid_week = current_week_num % 2 == 0
+    end
+
+    (dow == current_dow) && valid_week
+  end
+
+  def get_week(location)
+    week = :every
+
+    if location.has_key? 'week'
+      if location['week'] == 'odd'
+        week = :odd
+      end
+      if location['week'] == 'even'
+        week = :even
+      end
+    end
+
+    week
+  end
+end
