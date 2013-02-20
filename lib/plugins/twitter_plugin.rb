46fcb6d6a8c9a1244a8cd312dc7dcb684ec8cb16
Chris Searle <chris@chrissearle.org>
2013-02-20 13:40:17 +0100

    Add tweet function

--- lib/plugins/twitter_plugin.rb
+++ lib/plugins/twitter_plugin.rb
@@ -5,6 +5,24 @@ class TwitterPlugin
 
   timer 300, method: :timed
 
+  match /tweet (.+)/
+
+  def usage_hint
+    config[:conf]["usage"]
+  end
+
+  def execute(m, message)
+    tweeters = config[:conf]['tweeters'].split ','
+
+    if tweeters.include?(m.user.nick)
+      config[:tweeter].tweet(message)
+
+      m.reply config[:conf]['tweet-owner'].format_string_with_hash({ :nick => m.user.nick })
+    else
+      m.reply config[:conf]['tweet-non-owner'].format_string_with_hash({ :nick => m.user.nick })
+    end
+  end
+
   def timed
     debug "Timed - check mentions"
 
