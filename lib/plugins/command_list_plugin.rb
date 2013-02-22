5b2e92052d520a86d3f5c522dbc88a3ec7fbac10
Chris Searle <chris@chrissearle.org>
2013-02-22 14:34:35 +0100

    Re-do command list

--- lib/plugins/command_list_plugin.rb
+++ lib/plugins/command_list_plugin.rb
@@ -3,15 +3,26 @@ class CommandListPlugin
 
   set :required_options, [:conf]
 
-  match /c/
+  match /help/, method: :list_plugins
+  match /plugins/, method: :list_plugins
+  match /usage (.+)/
 
   def usage_hint
     config[:conf]["usage"]
   end
 
-  def execute(m)
-    bot.plugins.each do |plugin|
-      m.reply plugin.usage_hint if plugin.respond_to? "usage_hint"
+  def list_plugins(m)
+    m.reply config[:conf]["help"].format_string_with_hash(
+                {
+                    :plugins => bot.plugins.select { |plugin| plugin.respond_to? "usage_hint" }.map { |plugin| plugin.class.name }.join(", ")
+                })
+  end
+
+  def execute(m, plugin_name)
+    p = bot.plugins.find { |plugin| plugin.class.name == plugin_name }
+
+    if p.respond_to? "usage_hint"
+      m.reply p.usage_hint
     end
   end
 end
