3fe7d50287015c043993fd3f0dd94a88a9811295
Chris Searle <chris@chrissearle.org>
2013-02-28 10:00:36 +0100

    Spec tests for string expander

--- lib/general/string_expander.rb
+++ lib/general/string_expander.rb
@@ -2,9 +2,11 @@ class String
   def format_string_with_hash(params)
     result = self
 
-    params.each do |key, val|
-      match = key.to_s.upcase
-      result = result.gsub(/#{match}/, val)
+    unless params.nil?
+      params.each do |key, val|
+        match = key.to_s.upcase
+        result = result.gsub(/#{match}/, val)
+      end
     end
 
     result
---
+++ spec/test_string_expander.rb
@@ -0,0 +1,29 @@
+require "#{File.dirname(__FILE__)}/../lib/general/string_expander"
+
+describe 'Expand string' do
+  context 'nil params' do
+    it 'should leave string untouched' do
+      'Hi there. This is a string with a KEY and a VALUE in it.'.format_string_with_hash(nil).should eq('Hi there. This is a string with a KEY and a VALUE in it.')
+    end
+  end
+  context 'no params' do
+    it 'should leave string untouched' do
+      'Hi there. This is a string with a KEY and a VALUE in it.'.format_string_with_hash({}).should eq('Hi there. This is a string with a KEY and a VALUE in it.')
+    end
+  end
+  context 'one param' do
+    it 'should leave string untouched' do
+      'Hi there. This is a string with a KEY and a VALUE in it.'.format_string_with_hash({:key => 'foo'}).should eq('Hi there. This is a string with a foo and a VALUE in it.')
+    end
+  end
+  context 'all params' do
+    it 'should leave string untouched' do
+      'Hi there. This is a string with a KEY and a VALUE in it.'.format_string_with_hash({:key => 'foo', :value => 'bar'}).should eq('Hi there. This is a string with a foo and a bar in it.')
+    end
+  end
+  context 'excess params' do
+    it 'should leave string untouched' do
+      'Hi there. This is a string with a KEY and a VALUE in it.'.format_string_with_hash({:key => 'foo', :value => 'bar', :there => 'zot'}).should eq('Hi there. This is a string with a foo and a bar in it.')
+    end
+  end
+end
